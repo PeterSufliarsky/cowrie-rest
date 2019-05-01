311213a03ea1d112d89e1483d6cd591feabfd04d
Peter Šufliarsky <3963179+petersufliarsky@users.noreply.github.com>
2019-05-01 15:23:17 +0200

    Initial commit

---
+++ src/main/java/sk/sufliarsky/peter/honeypot/cowrierest/CowrieRestApplication.java
@@ -0,0 +1,13 @@
+package sk.sufliarsky.peter.honeypot.cowrierest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowrieRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CowrieRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/sufliarsky/peter/honeypot/cowrierest/CowrieRestApplicationTests.java
@@ -0,0 +1,16 @@
+package sk.sufliarsky.peter.honeypot.cowrierest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CowrieRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
