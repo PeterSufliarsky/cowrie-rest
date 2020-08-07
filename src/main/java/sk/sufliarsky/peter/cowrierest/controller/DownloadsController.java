4e490124e9460479bbcae68dec2c1bb585e3beaf
Peter Šufliarsky <3963179+petersufliarsky@users.noreply.github.com>
2020-08-07 18:24:13 +0200

    Add Downloads controller. Enable retrieving downloads from cowrie.

---
+++ src/main/java/sk/sufliarsky/peter/cowrierest/controller/DownloadsController.java
@@ -0,0 +1,22 @@
+package sk.sufliarsky.peter.cowrierest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sk.sufliarsky.peter.cowrierest.service.DownloadsService;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping(path="/downloads")
+public class DownloadsController {
+
+    @Autowired
+    private DownloadsService downloadsService;
+
+    @GetMapping(path=("/{hash}"))
+    public ResponseEntity<Resource> getDownload(@PathVariable String hash) throws IOException {
+        return downloadsService.getDownloadedFile(hash);
+    }
+}
--- src/main/java/sk/sufliarsky/peter/cowrierest/service/DownloadsService.java
+++ src/main/java/sk/sufliarsky/peter/cowrierest/service/DownloadsService.java
@@ -1,10 +1,22 @@
 package sk.sufliarsky.peter.cowrierest.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import sk.sufliarsky.peter.cowrierest.entity.Download;
 import sk.sufliarsky.peter.cowrierest.repository.DownloadsRepository;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Service
@@ -13,7 +25,27 @@ public class DownloadsService {
     @Autowired
     private DownloadsRepository downloadsRepository;
 
+    @Value("${cowrie.downloads.path}")
+    private String downloadsPath;
+
     public List<Download> getDownloadsForSession(String sessionId) {
         return downloadsRepository.findBySession(sessionId);
     }
+
+    public ResponseEntity<Resource> getDownloadedFile(String hash) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        File file = new File(downloadsPath + '/' + hash);
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
+	}
 }
